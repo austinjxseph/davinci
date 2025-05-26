6e86dd2aaa678f8e789679077f06ee47d0732a53
Austin Joseph <124405650+austinjxseph@users.noreply.github.com>
2025-05-26 19:50:39 +0100

    lenis added

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ document
 document
   .querySelector('[data-nav-element="overlay"]')
   .addEventListener("click", toggleNavigation);
+
+function smoothScroll() {
+  document.addEventListener("DOMContentLoaded", function () {
+    if (window.innerWidth >= 992) {
+      const lenis = new Lenis({
+        smooth: true,
+        lerp: 0.1,
+        wheelMultiplier: 1,
+        infinite: false,
+      });
+      function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
+      requestAnimationFrame(raf);
+    }
+  });
+}
